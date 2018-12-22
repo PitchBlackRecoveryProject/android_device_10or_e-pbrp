0e60553f0bc46716b90160df504f341e4ef9c733
Manish4586 <manish.n.manish45@gmail.com>
2018-12-22 20:14:43 +0530

    10 or. E : pbrp bring up

---
+++ omni_e.mk
@@ -0,0 +1,23 @@
+# Specify phone tech before including full_phone
+$(call inherit-product, vendor/omni/config/gsm.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+# Charger
+PRODUCT_PACKAGES += \
+    charger_res_images
+
+# Encryption
+PRODUCT_PACKAGES += \
+    libcryptfs_hw
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := e
+PRODUCT_BRAND := 10. or
+PRODUCT_MODEL := E
+PRODUCT_MANUFACTURER := 10. or
+
+PRODUCT_NAME := omni_e
